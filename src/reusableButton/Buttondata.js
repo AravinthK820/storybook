f9ffe7af51a7fafb207b731d436d245850317573
AravinthK820 <aravinth.k@unifo.in>
2023-06-14 19:45:28 +0530

    ChromaticCheckForReviews

--- src/reusableButton/Buttondata.js
+++ src/reusableButton/Buttondata.js
@@ -4,7 +4,7 @@ import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt
 const buttonData = { 
   obj1:{
   onClick: () => {console.log('button clicked1')},
-  text: "ChromaticChecksfinaldkljfaskkl",
+  text: "ChromaticChecksfinal",
   disabled: false,
   //startIcon: <SentimentSatisfiedAltIcon />,
   //endIcon: <SentimentSatisfiedAltIcon />,
